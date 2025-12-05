d5ce5d4d8de2ccd84b2f0baa801e3822af2a5b29
Vamshee <vamsheedesai@gmail.com>
2025-12-05 15:04:33 +0530

    Tailwindcss integration done

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Login from './core/auth/Login'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+     <Login/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/core/auth/Login.tsx
@@ -0,0 +1,36 @@
+
+const Login = () => {
+    return (
+        <main>
+
+            {/* 
+        card
+        card-header
+        card-body
+        card-footer
+        */}
+
+            <div className="card">
+                <div className="card-header">
+                    Login
+                </div>
+                <div className="card-body">
+
+                    {/* Form Fields */}
+
+
+                </div>
+                <div className="card-footer">
+
+                    {/* Form Fields */}
+
+                </div>
+            </div>
+
+
+
+        </main>
+    )
+}
+
+export default Login
